8fe04cec6eb1490a2ba0bdd3108b1c5ac02de2d1
James Ng <james.ng@projapps.com>
2020-04-03 02:28:37 +0800

    Complete Stage #3

--- Phone Book/task/src/phonebook/Main.kt
+++ Phone Book/task/src/phonebook/Main.kt
@@ -1,8 +1,9 @@
 package phonebook
 
 import java.io.File
+import java.lang.Exception
 import java.lang.System.currentTimeMillis
-import java.util.concurrent.Callable
+import kotlin.math.ceil
 import kotlin.math.sqrt
 import kotlin.system.measureTimeMillis
 
@@ -10,23 +11,6 @@ data class Entry(val name: String, val phone: String)
 
 val directory = mutableListOf<Entry>()
 val find = mutableListOf<String>()
-var sorted = false
-
-class BubbleSort: Callable<Long> {
-    override fun call(): Long {
-        return measureTimeMillis {
-            for (i in directory.indices) {
-                for (j in 0 until directory.size - (i + 1)) {
-                    if (directory[j].name > directory[j + 1].name) {
-                        val temp = directory[j]
-                        directory[j] = directory[j + 1]
-                        directory[j + 1] = temp
-                    }
-                }
-            }
-        }
-    }
-}
 
 private fun loadDirectory(filename: String = "/Users/professional/Downloads/directory.txt") {
     File(filename).forEachLine {
@@ -47,18 +31,20 @@ private fun loadFind(filename: String = "/Users/professional/Downloads/find.txt"
 
 private fun doLinear(): Long {
     println("Start searching (linear search)...")
-    val result = linearSearch()
+    val mutable = mutableListOf<Entry>()
+    mutable.addAll(directory)
+    val result = linearSearch(mutable)
     printFoundTime(result.first, result.second, result.third)
     return result.third
 }
 
-private fun linearSearch(): Triple<Int, Int, Long> {
+private fun linearSearch(mutable: MutableList<Entry>): Triple<Int, Int, Long> {
     var found = 0
     var entries = 0
     val executionTime = measureTimeMillis {
         find.forEach {
-            for (i in directory.indices) {
-                if (directory[i].name == it) {
+            for (i in mutable.indices) {
+                if (mutable[i].name == it) {
                     found++
                     break
                 }
@@ -82,37 +68,38 @@ private fun getTimeString(executionTime: Long): String {
 
 private fun bubbleJump(timeout: Long) {
     println("Start searching (bubble sort + jump search)...")
-    val executionTime = if (sorted) 0 else bubbleSort(timeout)
+    val mutable = mutableListOf<Entry>()
+    mutable.addAll(directory)
+    val executionTime = measureTimeMillis { bubbleSort(mutable, timeout) }
     if (executionTime > timeout) {
-        val result = linearSearch()
+        val result = linearSearch(mutable)
         val linearTime = result.third
         printFoundTime(result.first, result.second, result.third + executionTime)
         println("Sorting time: ${getTimeString(executionTime)} - STOPPED, moved to linear search")
         println("Searching time: ${getTimeString(linearTime)}")
     } else {
-        sorted = true
-        val result = jumpSearch()
+        val result = jumpSearch(mutable)
         printFoundTime(result.first, result.second, result.third + executionTime)
         println("Sorting time: ${getTimeString(executionTime)}")
         println("Searching time: ${getTimeString(result.third)}")
     }
 }
 
-private fun jumpSearch(): Triple<Int, Int, Long> {
+private fun jumpSearch(mutable: MutableList<Entry>): Triple<Int, Int, Long> {
     var found = 0
     var entries = 0
     val executionTime = measureTimeMillis {
-        val jump = sqrt(directory.size.toDouble()).toInt()
+        val jump = sqrt(mutable.size.toDouble()).toInt()
         find.forEach {
             var index = 0
-            if (directory[index].name == it) {
+            if (mutable[index].name == it) {
                 found++
-            } else if (directory[index].name < it) {
+            } else if (mutable[index].name < it) {
                 var prev = index + 1
                 index += jump
-                loop@ while (index < directory.size && directory[prev - 1].name < it) {
+                loop@ while (index < mutable.size && mutable[prev - 1].name < it) {
                     for (i in index downTo prev) {
-                        if (directory[i].name == it) {
+                        if (mutable[i].name == it) {
                             found++
                             break@loop
                         }
@@ -120,9 +107,9 @@ private fun jumpSearch(): Triple<Int, Int, Long> {
                     prev = index + 1
                     index += jump
                 }
-                if (directory[prev - 1].name < it && index > directory.size - 1) {
-                    for (i in directory.size - 1 downTo prev) {
-                        if (directory[i].name == it) {
+                if (mutable[prev - 1].name < it && index > mutable.size - 1) {
+                    for (i in mutable.size - 1 downTo prev) {
+                        if (mutable[i].name == it) {
                             found++
                         }
                     }
@@ -134,25 +121,96 @@ private fun jumpSearch(): Triple<Int, Int, Long> {
     return Triple(found, entries, executionTime)
 }
 
-private fun bubbleSort(timeout: Long): Long {
-    return measureTimeMillis {
-        val start = currentTimeMillis()
-        var n = directory.size
-        var swapped = true
-        while (swapped) {
-            swapped = false
-            for (i in 1 until n) {
-                if (directory[i - 1].name > directory[i].name) {
-                    val temp = directory[i]
-                    directory[i] = directory[i - 1]
-                    directory[i - 1] = temp
-                    swapped = true
-                }
+private fun bubbleSort(mutable: MutableList<Entry>, timeout: Long) {
+    val start = currentTimeMillis()
+    var n = mutable.size
+    var swapped = true
+    while (swapped) {
+        swapped = false
+        for (i in 1 until n) {
+            if (mutable[i - 1].name > mutable[i].name) {
+                val temp = mutable[i]
+                mutable[i] = mutable[i - 1]
+                mutable[i - 1] = temp
+                swapped = true
             }
-            n--
-            if (timeout > 0 && currentTimeMillis() - start > timeout) break
+        }
+        n--
+        if (timeout > 0 && currentTimeMillis() - start > timeout) break
+    }
+}
+
+private fun quickBinary(timeout: Long) {
+    println("Start searching (quick sort + binary search)...")
+    val mutable = mutableListOf<Entry>()
+    mutable.addAll(directory)
+    val executionTime = measureTimeMillis {
+        try {
+            quickSort(mutable, 0, mutable.size - 1, timeout, currentTimeMillis())
+        } catch (e: Exception) {
+            // Timeout triggered
         }
     }
+    if (executionTime > timeout) {
+        val result = linearSearch(mutable)
+        val linearTime = result.third
+        printFoundTime(result.first, result.second, result.third + executionTime)
+        println("Sorting time: ${getTimeString(executionTime)} - STOPPED, moved to linear search")
+        println("Searching time: ${getTimeString(linearTime)}")
+    } else {
+        val result = binarySearch(mutable)
+        printFoundTime(result.first, result.second, result.third + executionTime)
+        println("Sorting time: ${getTimeString(executionTime)}")
+        println("Searching time: ${getTimeString(result.third)}")
+    }
+}
+
+private fun binarySearch(mutable: MutableList<Entry>): Triple<Int, Int, Long> {
+    var found = 0
+    var entries = 0
+    val executionTime = measureTimeMillis {
+        find.forEach {
+            var l = 0
+            var r = mutable.size - 1
+            while (l != r) {
+                val m = ceil((l + r).toDouble() / 2).toInt()
+                if (mutable[m].name > it)
+                    r = m - 1
+                else
+                    l = m
+            }
+            if (mutable[l].name == it)
+                found++
+            entries++
+        }
+    }
+    return Triple(found, entries, executionTime)
+}
+
+private fun quickSort(mutable: MutableList<Entry>, lo: Int, hi: Int, timeout: Long, start: Long) {
+    if (lo < hi) {
+        val p = partition(mutable, lo, hi)
+        if (timeout > 0 && currentTimeMillis() - start > timeout) throw Exception("Timeout")
+        quickSort(mutable, lo, p - 1, timeout, start)
+        quickSort(mutable, p + 1, hi, timeout, start)
+    }
+}
+
+private fun partition(mutable: MutableList<Entry>, lo: Int, hi: Int): Int {
+    val pivot = mutable[hi].name
+    var i = lo
+    for (j in lo until hi) {
+        if (mutable[j].name < pivot) {
+            val temp = mutable[i]
+            mutable[i] = mutable[j]
+            mutable[j] = temp
+            i++
+        }
+    }
+    val temp = mutable[i]
+    mutable[i] = mutable[hi]
+    mutable[hi] = temp
+    return i
 }
 
 fun main() {
@@ -161,4 +219,6 @@ fun main() {
     val linearTime = doLinear()
     println()
     bubbleJump(linearTime * 10)
+    println()
+    quickBinary(linearTime * 10)
 }
